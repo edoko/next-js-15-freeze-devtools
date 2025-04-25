ab20f5f6668371465784a7552d796a193e8cecfb
Kevin Park <koma@komalab.io>
2025-04-25 10:56:06 +0900

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  allowedDevOrigins: ['10.10.0.13'],
+};
+
+export default nextConfig;
